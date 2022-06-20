6c2215673e2a6b3875921f6f82c79da2c2549635
Özay Akcan <79650791+ozayakcan@users.noreply.github.com>
2022-06-20 23:49:30 +0000

    Response find method changed to difflib.get_close_matches

--- config/server.py
+++ config/server.py
@@ -3,6 +3,7 @@ from config.strings import HandleStrings
 from flask import Flask, request
 import random
 import json
+import difflib
 
 app = Flask(AIConfig.app_name)
 
@@ -26,11 +27,18 @@ def talk():
   response_found = False
   if lang in locale:
     for langJ in locale[lang]:
-      if HandleStrings.get_data(data, "message", "message") in langJ["patterns"]:
+      matches = difflib.get_close_matches(HandleStrings.get_data(data, "message", "message"), langJ["patterns"])
+      if len(matches) > 0:
         response["message"] = HandleStrings.replaceStrings(data, random.choice(langJ["responses"]))
         response["id"] = langJ["id"]
         response_found = True
         break
+#      if HandleStrings.get_data(data, "message", "message") in langJ["patterns"]:
+#        response["message"] = HandleStrings.replaceStrings(data, random.choice(langJ["responses"]))
+#        response["id"] = langJ["id"]
+#        response_found = True
+#        break
+        
   if response_found:
     return str(response)
   else:
